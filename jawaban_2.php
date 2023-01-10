11afb226966ecb6e6d487f041368e135bc8844a2
furqoncoding <77562258+furqoncoding@users.noreply.github.com>
2023-01-10 15:06:24 +0700

    Add files via upload

---
+++ jawaban_2.php
@@ -0,0 +1,108 @@
+<!DOCTYPE>
+<html>
+    <head>
+
+        <style type="text/css">
+            body {background-color: powderblue;}
+            h1, h2, h3, h4, h5, h6 {
+                font-family: Cursive;
+                color: maroon;
+            }
+            .square {
+              width: 50px;
+              height: 50px;
+              margin: 5px;
+              -ms-transform: rotate(-45deg);
+              -webkit-transform: rotate(-45deg);
+              transform: rotate(-45deg);
+              overflow: hidden;
+            }
+            .pic {
+              background: "";
+              width: 150px;
+              height: 150px;
+              margin: -25px;
+              background-color: #006400;
+              -ms-transform: rotate(45deg);
+              -webkit-transform: rotate(45deg);
+              transform: rotate(45deg);
+            }
+            .square2 {
+              width: 50px;
+              height: 50px;
+              margin: 5px;
+              -ms-transform: rotate(-45deg);
+              -webkit-transform: rotate(-45deg);
+              transform: rotate(-45deg);
+              overflow: hidden;
+            }
+            .pic2 {
+              background: "";
+              width: 150px;
+              height: 150px;
+              margin: -25px;
+              background-color: #ADFF2F;
+              -ms-transform: rotate(45deg);
+              -webkit-transform: rotate(45deg);
+              transform: rotate(45deg);
+            }
+            .row {
+                display: flex;
+                flex-direction: row;     /* make main axis horizontal (default setting) */
+                justify-content: center; /* center items horizontally, in this case */
+                align-items: center; 
+            }
+            .space {
+                width: 29px;
+            }
+        </style>
+
+
+
+
+        <script>  
+
+        </script>
+
+        
+    </head> 
+    <body>
+        
+        
+        <br>
+        <br>
+        <br>
+        <center>
+            <div class="square"><div class="pic"></div></div>
+        </center>
+        
+        <div class="row">
+            <div class="square2"><div class="pic2"></div></div>
+            <div class="space"></div>
+            <div class="square2"><div class="pic2"></div></div>
+        </div>
+        
+        <div class="row">
+            <div class="square"><div class="pic"></div></div>
+            <div class="space"></div>
+            <div class="square"><div class="pic"></div></div>
+            <div class="space"></div>
+            <div class="square"><div class="pic"></div></div>
+        </div>
+        
+        <div class="row">
+            <div class="square2"><div class="pic2"></div></div>
+            <div class="space"></div>
+            <div class="square2"><div class="pic2"></div></div>
+        </div>
+        
+        <center>
+            <div class="square"><div class="pic"></div></div>
+        </center>
+        
+        <br>
+        <center><h2>〰️ Selamat Hari Raya Idul Fitri 〰️</h2></center>
+        <center><h2>Mohon maaf lahir dan batin ❤️</h2></center>
+
+    </body>
+</html>
